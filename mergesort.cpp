a468abf8ae1c34f72c37bc200eb716dea585b7a8
jojelupipa <jesus_38b@hotmail.com>
2017-03-14 13:58:06 +0100

    Añadidos todos los gráficos y datos de mergesort. No me cae bien gnuplot.

--- mergesort.cpp
+++ mergesort.cpp
@@ -22,7 +22,7 @@ using namespace std;
    @brief Ordena un vector por el método de mezcla.
 
    @param T: vector de elementos. Debe tener num_elem elementos.
-             Es MODIFICADO.
+   Es MODIFICADO.
    @param num_elem: número de elementos. num_elem > 0.
 
    Cambia el orden de los elementos de T de forma que los dispone
@@ -38,12 +38,12 @@ void mergesort(int T[], int num_elem);
    @brief Ordena parte de un vector por el método de mezcla.
 
    @param T: vector de elementos. Tiene un número de elementos 
-                   mayor o igual a final. Es MODIFICADO.
+   mayor o igual a final. Es MODIFICADO.
    @param inicial: Posición que marca el incio de la parte del
-                   vector a ordenar.
+   vector a ordenar.
    @param final: Posición detrás de la última de la parte del
-                   vector a ordenar. 
-		   inicial < final.
+   vector a ordenar. 
+   inicial < final.
 
    Cambia el orden de los elementos de T entre las posiciones
    inicial y final - 1 de forma que los dispone en sentido creciente
@@ -57,7 +57,7 @@ static void mergesort_lims(int T[], int inicial, int final);
    @brief Ordena un vector por el método de inserción.
 
    @param T: vector de elementos. Debe tener num_elem elementos.
-             Es MODIFICADO.
+   Es MODIFICADO.
    @param num_elem: número de elementos. num_elem > 0.
 
    Cambia el orden de los elementos de T de forma que los dispone
@@ -72,12 +72,12 @@ void insercion(int T[], int num_elem);
    @brief Ordena parte de un vector por el método de inserción.
 
    @param T: vector de elementos. Tiene un número de elementos 
-                   mayor o igual a final. Es MODIFICADO.
+   mayor o igual a final. Es MODIFICADO.
    @param inicial: Posición que marca el incio de la parte del
-                   vector a ordenar.
+   vector a ordenar.
    @param final: Posición detrás de la última de la parte del
-                   vector a ordenar. 
-		   inicial < final.
+   vector a ordenar. 
+   inicial < final.
 
    Cambia el orden de los elementos de T entre las posiciones
    inicial y final - 1 de forma que los dispone en sentido creciente
@@ -91,16 +91,16 @@ static void insercion_lims(int T[], int inicial, int final);
    @brief Mezcla dos vectores ordenados sobre otro.
 
    @param T: vector de elementos. Tiene un número de elementos 
-                   mayor o igual a final. Es MODIFICADO.
+   mayor o igual a final. Es MODIFICADO.
    @param inicial: Posición que marca el incio de la parte del
-                   vector a escribir.
+   vector a escribir.
    @param final: Posición detrás de la última de la parte del
-                   vector a escribir
-		   inicial < final.
+   vector a escribir
+   inicial < final.
    @param U: Vector con los elementos ordenados.
    @param V: Vector con los elementos ordenados.
-             El número de elementos de U y V sumados debe coincidir
-             con final - inicial.
+   El número de elementos de U y V sumados debe coincidir
+   con final - inicial.
 
    En los elementos de T entre las posiciones inicial y final - 1
    pone ordenados en sentido creciente, de menor a mayor, los
@@ -150,27 +150,27 @@ static void mergesort_lims(int T[], int inicial, int final)
     {
       insercion_lims(T, inicial, final);
     } else {
-      int k = (final - inicial)/2;
+    int k = (final - inicial)/2;
 
-      int * U = new int [k - inicial + 1];
-      assert(U);
-      int l, l2;
-      for (l = 0, l2 = inicial; l < k; l++, l2++)
-	U[l] = T[l2];
-      U[l] = INT_MAX;
+    int * U = new int [k - inicial + 1];
+    assert(U);
+    int l, l2;
+    for (l = 0, l2 = inicial; l < k; l++, l2++)
+      U[l] = T[l2];
+    U[l] = INT_MAX;
 
-      int * V = new int [final - k + 1];
-      assert(V);
-      for (l = 0, l2 = k; l < final - k; l++, l2++)
-	V[l] = T[l2];
-      V[l] = INT_MAX;
+    int * V = new int [final - k + 1];
+    assert(V);
+    for (l = 0, l2 = k; l < final - k; l++, l2++)
+      V[l] = T[l2];
+    V[l] = INT_MAX;
 
-      mergesort_lims(U, 0, k);
-      mergesort_lims(V, 0, final - k);
-      fusion(T, inicial, final, U, V);
-      delete [] U;
-      delete [] V;
-    };
+    mergesort_lims(U, 0, k);
+    mergesort_lims(V, 0, final - k);
+    fusion(T, inicial, final, U, V);
+    delete [] U;
+    delete [] V;
+  };
 }
   
 
@@ -229,36 +229,36 @@ int main(int argc, char * argv[])
       cout << n << "  " << ((double)(t_despues - t_antes)) / CLOCKS_PER_SEC 
 	   << endl;
     } else {
-      int * U = new int[n];
-      assert(U);
+    int * U = new int[n];
+    assert(U);
 
-      for (int i = 0; i < n; i++)
-	U[i] = T[i];
+    for (int i = 0; i < n; i++)
+      U[i] = T[i];
       
-      clock_t t_antes_vacio = clock();
-      for (int veces = 0; veces < NUM_VECES; veces++)
-	{
-	  for (int i = 0; i < n; i++)
-	    U[i] = T[i];
-	}
-      clock_t t_despues_vacio = clock();
+    clock_t t_antes_vacio = clock();
+    for (int veces = 0; veces < NUM_VECES; veces++)
+      {
+	for (int i = 0; i < n; i++)
+	  U[i] = T[i];
+      }
+    clock_t t_despues_vacio = clock();
 
-      clock_t t_antes = clock();
-      for (int veces = 0; veces < NUM_VECES; veces++)
-	{
-	  for (int i = 0; i < n; i++)
-	    U[i] = T[i];
-	  mergesort(U, n);
-	}
-      clock_t t_despues = clock();
-      cout << n << " \t  " 
-	   << ((double) ((t_despues - t_antes) - 
-			 (t_despues_vacio - t_antes_vacio))) / 
-	(CLOCKS_PER_SEC * NUM_VECES)
-	   << endl;
+    clock_t t_antes = clock();
+    for (int veces = 0; veces < NUM_VECES; veces++)
+      {
+	for (int i = 0; i < n; i++)
+	  U[i] = T[i];
+	mergesort(U, n);
+      }
+    clock_t t_despues = clock();
+    cout << n << " \t  " 
+	 << ((double) ((t_despues - t_antes) - 
+		       (t_despues_vacio - t_antes_vacio))) / 
+      (CLOCKS_PER_SEC * NUM_VECES)
+	 << endl;
 
-      delete [] U;
-    }
+    delete [] U;
+  }
 
 
   delete [] T;
