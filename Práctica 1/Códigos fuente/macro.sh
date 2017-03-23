2de714be63bef0a233f7cf13b076861f4672711c
amartinruiz23 <amr.antonio42@gmail.com>
2017-03-23 18:14:28 +0100

    Macromacro

--- Práctica 1/Códigos fuente/macro.sh
+++ Práctica 1/Códigos fuente/macro.sh
@@ -2,6 +2,42 @@
 #echo "" >> salida.dat
 @ i = 0
 while ( $i <= 50000 )
-./heapsort $i >> heapsort_OPTIMIZADO.dat
+./burbuja $i >> burbuja_comparable.dat
+
+@ i += 1000
+end
+
+@ i = 0
+while ( $i <= 50000 )
+./heapsort $i >> heapsort_comparable.dat
+
+@ i += 1000
+end
+
+@ i = 0
+while ( $i <= 50000 )
+./insercion $i >> insercion_comparable.dat
+
+@ i += 1000
+end
+
+@ i = 0
+while ( $i <= 50000 )
+./mergesort $i >> mergesort_comparable.dat
+
+@ i += 1000
+end
+
+@ i = 0
+while ( $i <= 50000 )
+./seleccion $i >> seleccion_comparable.dat
+
+@ i += 1000
+end
+
+@ i = 0
+while ( $i <= 50000 )
+./quicksort $i >> quicksort_comparable.dat
+
 @ i += 1000
 end
