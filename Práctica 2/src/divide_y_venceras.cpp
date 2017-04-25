bc198fe9502f45e7df7ac12f7320327a3e5411ac
jojelupipa <jesus_38b@hotmail.com>
2017-04-25 21:55:05 +0200

    P2:Arreglado un pequeño grave errata que no dividía los vectores

--- Práctica 2/src/divide_y_venceras.cpp
+++ Práctica 2/src/divide_y_venceras.cpp
@@ -5,52 +5,52 @@ using namespace std;
 #include <climits>
 #include <cassert>
 
-//generador de ejemplos para el problema de la comparaci�n de preferencias. Simplemente se genera una permutaci�n aleatoria del vector 0,1,2,...,n-2,n-1
+//generador de ejemplos para el problema de la comparación de preferencias. Simplemente se genera una permutación aleatoria del vector 0,1,2,...,n-2,n-1
 
-double uniforme() //Genera un n�mero uniformemente distribuido en el
-                  //intervalo [0,1) a partir de uno de los generadores
-                  //disponibles en C.
+double uniforme() //Genera un número uniformemente distribuido en el
+//intervalo [0,1) a partir de uno de los generadores
+//disponibles en C.
 {
- int t = rand();
- double f = ((double)RAND_MAX+1.0);
- return (double)t/f;
+  int t = rand();
+  double f = ((double)RAND_MAX+1.0);
+  return (double)t/f;
 }
 
 
 int contarInversiones( int * preferencias1, int * preferencias2, const int TAM) {
 
-	int total = 0 ;
-	for (int i = 0 ; i < TAM ; i++) {
-		int j = 0 ;
-		bool menor = true ;
-		while (j < TAM && menor) {
-			if (preferencias2 [i%TAM] < preferencias1 [j]) {
-				j++;
-				total++;
-			}
-			else
-				menor = false;
-		}
-	}
-	return total;
+  int total = 0 ;
+  for (int i = 0 ; i < TAM ; i++) {
+    int j = 0 ;
+    bool menor = true ;
+    while (j < TAM && menor) {
+      if (preferencias2 [i%TAM] < preferencias1 [j]) {
+	j++;
+	total++;
+      }
+      else
+	menor = false;
+    }
+  }
+  return total;
 }
 
 int comparaPreferencias(int * v , const int TAM ) {
-	if (TAM == 0)
-		return 0 ;
+  if (TAM == 0)
+    return 0 ;
 
-	const int MITAD = TAM /2 ;
-	int * preferencias1 = new int [MITAD];
-	int * preferencias2 = new int [MITAD];
-	preferencias1 = v ;
+  const int MITAD = TAM /2 ;
+  int * preferencias1 = new int [MITAD];
+  int * preferencias2 = &v[MITAD];
+  preferencias1 = v ;
 
-	int pref1 = comparaPreferencias(preferencias1, MITAD);
-	int pref2 = comparaPreferencias(preferencias2, MITAD);
-	int total = pref1 + pref2 ;
+  int pref1 = comparaPreferencias(preferencias1, MITAD);
+  int pref2 = comparaPreferencias(preferencias2, MITAD);
+  int total = pref1 + pref2 ;
 
-	total += contarInversiones(preferencias1, preferencias2, MITAD);
+  total += contarInversiones(preferencias1, preferencias2, MITAD);
 
-	return total ;
+  return total ;
 
 }
 
@@ -61,8 +61,8 @@ int comparaPreferencias(int * v , const int TAM ) {
 int main(int argc, char * argv[]) {
 
   if (argc != 2) {
-      cerr << "Formato " << argv[0] << " <num_elem>" << endl;
-      return -1;
+    cerr << "Formato " << argv[0] << " <num_elem>" << endl;
+    return -1;
   }
 
   int n = atoi(argv[1]);
@@ -82,16 +82,16 @@ int main(int argc, char * argv[]) {
     T[k] = tmp;
   }
 	
-	clock_t tantes;
-	clock_t tdespues;
-	int invertidos = 0 ;
+  clock_t tantes;
+  clock_t tdespues;
+  int invertidos = 0 ;
 
-	tantes = clock();
-	invertidos = comparaPreferencias(T,n);
-	tdespues = clock();
+  tantes = clock();
+  invertidos = comparaPreferencias(T,n);
+  tdespues = clock();
 
-	cout << n << " " << (double)(tdespues-tantes)/CLOCKS_PER_SEC << endl ;
-	delete [] T ;
+  cout << n << " " << (double)(tdespues-tantes)/CLOCKS_PER_SEC << endl ;
+  delete [] T ;
 
 
 }
