559f923c7852bafcaf497efa953bf2abc158e0a8
Álvaro López <alopezj97@gmail.com>
2017-05-17 10:53:03 +0200

    ahora se puede ejecutar

--- Práctica 3/recubrimiento.cpp
+++ Práctica 3/recubrimiento.cpp
@@ -40,7 +40,9 @@ using namespace std;
 
 vector<pair<int, int> > recubrimiento ( vector<vector<int> > m , int N) {
 
+	cout << "dentro de recubrimiento" << endl ;
 	vector<pair<int, int> > sol ;
+	/*
 	int nodo_actual = 0 ;
 
 	for (int f = 0 ; f < N ; ++f) {
@@ -52,7 +54,7 @@ vector<pair<int, int> > recubrimiento ( vector<vector<int> > m , int N) {
 			}
 		}
 	}
-
+	*/
 	return sol ;
 }
 
@@ -68,42 +70,34 @@ int main (int argc, char* argv[]) {
 	int tamanio ;
 	int dato ;
 	vector<vector<int> > matriz ; // Se puede optimizar
-	cout << "a" << endl;
 	ifstream flujo (archivo) ;
-
-	matriz.resize(tamanio);
-	for (int i = 0; i < tamanio; i++)
-		matriz[i].resize(tamanio);
-
-	cout << "b" << endl;
+	
 	if (flujo) {
-		cout << "c" << endl;
 		flujo >> tamanio ;
+		matriz.resize(tamanio);
+		for (int i = 0; i < tamanio; i++)
+			matriz[i].resize(tamanio);
 		cout <<"TAM: "<< tamanio << endl;
-		char basura ;
-
-
+	
 		for (int f = 0; (f < tamanio) && flujo; ++f) {
-			flujo >> basura ; cout << "c2" <<endl;
 			for (int c = 0; (c < tamanio) && flujo; ++c) {		// Se puede optimizar
-				flujo >> dato ; cout << "c3"<<endl;
-				cout <<"DATO: "<<dato << endl;
-				matriz[f][c] = dato; cout << "c4"<<endl;
-				flujo >> basura;
+				flujo >> dato ; 
+				//cout <<"DATO: "<<dato << endl;
+				matriz[f][c] = dato; 
 			}
 		}
-		cout << "c1" << endl;
+	
 	}
 	else {
 		cout << "Error al abrir el fichero" << endl ;
 		return (-2);
 	}
 
-	cout << "d" << endl;
 	vector<pair<int, int> > solucion = recubrimiento ( matriz, tamanio ) ;
 
+	/*
 	for (int i = 0; i < tamanio; ++i)
 			cout << "(" << solucion[i].first <<", "<<solucion[i].second << ")\t";
-
+	*/
 
 }
